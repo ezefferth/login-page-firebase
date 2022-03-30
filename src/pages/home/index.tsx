d056fbbb75f8c175899ba544cdfd2caa51c9d4cf
ezefferth <ezefferth96@gmail.com>
2022-03-30 18:07:40 -0300

    start with change layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,10 +4,35 @@ import { useAuth } from '../../components/context/authContext';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './home.module.scss';
 
 export default function Home() {
+
+
+  const [about, setAbout] = useState<boolean>(false);
+  const [aboutMe, setAboutMe] = useState<boolean>(false);
+  const [loginPageApp, setLoginPageApp] = useState<boolean>(true);
+
+  const handleLoginPage = () => {
+    setAbout(false);
+    setAboutMe(false);
+    setLoginPageApp(true);
+  }
+
+  const handleAbout = () => {
+    setAbout(true);
+    setAboutMe(false);
+    setLoginPageApp(false);
+
+  }
+
+  const handleAboutMe = () => {
+    setAbout(false);
+    setAboutMe(true);
+    setLoginPageApp(false);
+
+  }
 
   const {
     currentUser,
@@ -31,36 +56,58 @@ export default function Home() {
 
 
   return (
-    <div>
+    <div className={styles.container}>
       <Head>{/* title da aba */}
         <title>Home Page</title>
       </Head>
       <nav className={`navbar navbar-expandlg fixed-top navbar-dark bg-dark`}>
         <div className={`container-fluid ${styles.containerNav}`}>
-          <span className={`navbar-brand`}>Login Page App</span>
+          <span
+            className={`navbar-brand ${styles.loginPageApp}`}
+            onClick={handleLoginPage}
+          >Login Page App</span>
 
 
           <div className={styles.dados}>
 
-            <span className={styles.aboutMe}>Sobre mim</span>
+            <span
+              className={styles.aboutMe}
+              onClick={handleAboutMe}
+            >Sobre mim</span>
 
-            <span className={styles.about}>Sobre a aplicação</span>
+            <span
+              className={styles.about}
+              onClick={handleAbout}
+            >Sobre a aplicação</span>
 
             <span className={styles.sair}
               onClick={Logout}
             >Sair</span>
           </div>
 
-
-
-
-
         </div>
       </nav>
-      {/* <h1>HOME</h1>
-      <button onClick={Logout}>
-        logout
-      </button> */}
+
+      {loginPageApp && (
+        <div className={styles.bemVindo}>
+          <p>Bem Vindo!</p>
+        </div>
+      )}
+      {about && (
+        <div className={styles.containerDiv}>
+          <h1>about</h1>
+
+        </div>
+      )}
+      {aboutMe && (
+        <div className={styles.containerDiv}>
+          <h1>aboutMe</h1>
+          <h1>aboutMe</h1>
+          <h1>aboutMe</h1>
+          <h1>aboutMe</h1>
+
+        </div>
+      )}
     </div>
   )
 }
